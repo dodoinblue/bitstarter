056bb1cbb94eca077d24819adac9e12e830aa5c2
Charles <dodoinblue@gmail.com>
2013-07-07 05:58:03 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+buf=fs.readFileSync('index.html');
+data=buf.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 3000;
